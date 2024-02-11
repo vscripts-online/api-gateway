fe31c537a702b71dd001fcedca681479b2f318c2
Erdem <erdemefesteam2@hotmail.com>
2024-02-11 17:10:58 +0300

    swagger installed

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,25 @@ config();
 process.on('uncaughtException', (err) => {
   console.log('uncaughtException');
   console.log(err);
-  console.log(err.stack);
 });
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './common/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('vscripts.online/cdn')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableShutdownHooks();
   app.enableCors({ origin: '*' });
