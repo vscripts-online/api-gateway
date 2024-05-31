af0ea59df7a2b0452bc19cf242aad31de6d975c7
Erdem <22412769+virtueer@users.noreply.github.com>
2024-05-31 17:42:37 +0300

    Auth.setMetadata added

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -25,6 +25,15 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
 
+    if (user.metadata.total === undefined || user.metadata.used === undefined) {
+      const metadata = await this.authService.setMetadata(user.id);
+      if (!metadata) {
+        return false;
+      }
+
+      user.metadata = metadata;
+    }
+
     request['user'] = user;
 
     return true;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,47 @@
 import { Injectable } from '@nestjs/common';
-import { AUTH_CLIENT_ID, AUTH_HOST_URI } from 'src/common/config';
+import * as bytes from 'bytes';
+import {
+  AUTH_CLIENT_ID,
+  AUTH_CLIENT_SECRET,
+  AUTH_HOST_URI,
+} from 'src/common/config';
+
+interface IUserMetadata {
+  admin?: boolean;
+  total?: number;
+  used?: number;
+}
+
+interface IGetMeResponse {
+  status: boolean;
+  user?: {
+    avatar: string;
+    email: string;
+    has_password: boolean;
+    id: number;
+    name: string;
+    role: 'USER' | 'ADMIN';
+    metadata?: IUserMetadata;
+  };
+}
+
+interface ISetMetadataFailResponse {
+  status: false;
+  message: string;
+}
+
+interface ISetMetadataSuccessResponse {
+  user: {
+    id: number;
+    userId: number;
+    appId: string;
+    metadata: IUserMetadata;
+  };
+}
+
+type SetMetadataResponse =
+  | ISetMetadataFailResponse
+  | ISetMetadataSuccessResponse;
 
 @Injectable()
 export class AuthService {
@@ -13,12 +55,35 @@ export class AuthService {
       },
     });
 
-    const data = await response.json();
+    const data: IGetMeResponse = await response.json();
 
     if (!data.status) {
       return false;
     }
 
     return data.user;
+  }
+
+  async setMetadata(id: number) {
+    const url = new URL(AUTH_HOST_URI + '/api/app/user');
+    url.searchParams.set('client_id', AUTH_CLIENT_ID);
+    url.searchParams.set('client_secret', AUTH_CLIENT_SECRET);
+    url.searchParams.set('user', id + '');
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ total: bytes('500mb'), used: 0 } as IUserMetadata),
+    });
+
+    const data: SetMetadataResponse = await response.json();
+
+    if (!(data as ISetMetadataFailResponse).status) {
+      return false;
+    }
+
+    return (data as ISetMetadataSuccessResponse).user.metadata;
   }
 }
